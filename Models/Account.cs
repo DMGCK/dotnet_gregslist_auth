efecc21292987eae6f17fdf4b81db87278fe30fe
cameorn <cameron.kueneman@gmail.com>
2022-07-19 14:33:58 -0600

    project initialized

---
+++ Models/Account.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace dotnet_gregslist_auth.Models
+{
+    public class Account
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Picture { get; set; }
+    }
+}
